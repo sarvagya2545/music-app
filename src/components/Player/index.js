8cef7d1356471d1bc5d06649d174934234c39ce9
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-07-01 00:21:31 +0530

    timer finished

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -3,14 +3,21 @@ import { GlassContainer } from '../../styles/globalStyles';
 import { PlayerControls, ControlIcon, AudioTrack, Time, TotalTime } from './Player.elements';
 import { FaPlay, FaPause, FaFastForward, FaFastBackward } from 'react-icons/fa';
 
-const audioSrc = '/music/ATTACK ON TITAN - Full English Opening 1 (Guren No Yumiya) Cover by Jonathan Young feat.m4a';
+const audioSrc = '/music/Bring Me To Life - Evanescence (Cover by Jonathan Young).m4a';
 
 const Player = () => {
+    const audioRef = useRef(new Audio(audioSrc));
+    const intervalRef = useRef();
+
     const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTrack, setCurrentTrack] = useState(0);
+    const [trackLength, setTrackLength] = useState(100);
+
+    audioRef.current.addEventListener('loadedmetadata', e => {
+        setTrackLength(audioRef.current.duration);
+    })
 
     useEffect(() => {
-        // setIsPlaying(true);
-        console.log(audioRef.current);
         if(audioRef) {
             if(isPlaying) {
                 audioRef.current.play()
@@ -18,13 +25,33 @@ const Player = () => {
                         console.log('Player started');
                     })
                     .catch(err => console.log(err))
+                ;
+
+                intervalRef.current = setInterval(() => {
+                    if (audioRef.current.ended) {
+                        // toNextTrack();
+                        clearInterval(intervalRef.current);
+                        // console.log('Ended');
+                    } else {
+                        setCurrentTrack(audioRef.current.currentTime);
+                        // console.log(audioRef.current.currentTime);
+                    }
+                }, 1000);
+
             } else {
                 audioRef.current.pause()
+                clearInterval(intervalRef.current);
             }
         };
-    }, [isPlaying])
+    }, [isPlaying]);
 
-    const audioRef = useRef(new Audio(audioSrc));
+    const convertTime = (seconds) => {
+        return `${Math.floor(seconds / 60)}:${
+            Math.ceil(seconds % 60).toLocaleString('en-US', {
+                minimumIntegerDigits: 2
+            })
+        }`;
+    }
 
     const togglePlay = () => setIsPlaying(s => !s);
 
@@ -41,8 +68,8 @@ const Player = () => {
                     <FaFastForward />
                 </ControlIcon>
             </PlayerControls>
-            <AudioTrack progress="20" value="20"/>
-            <Time>01:00<TotalTime> / 03:04</TotalTime></Time>
+            <AudioTrack progress={currentTrack / trackLength * 100} value={currentTrack} min="0" max={trackLength}/>
+            <Time>{convertTime(currentTrack)}<TotalTime> / {convertTime(trackLength)}</TotalTime></Time>
         </GlassContainer>
     );
 }
